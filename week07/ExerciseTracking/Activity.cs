294255b523e833a0a4cf3104995ac46f99a659b2
Adafa Ralph <yme7ir@gmail.com>
2026-02-17 18:49:37 -0400

    week 7 finally

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,29 @@
+namespace ExerciseTracking
+{
+    public abstract class Activity
+    {
+        private string _date;
+        private int _minutes;
+
+        public Activity(string date, int minutes)
+        {
+            _date = date;
+            _minutes = minutes;
+        }
+
+        protected int GetMinutes() => _minutes;
+        protected string GetDate() => _date;
+
+        public abstract double GetDistance();
+        public abstract double GetSpeed();
+        public abstract double GetPace();
+
+        public virtual string GetSummary()
+        {
+            return $"{GetDate()} {this.GetType().Name} ({GetMinutes()} min): " +
+                   $"Distance {GetDistance():0.0} miles, " +
+                   $"Speed {GetSpeed():0.0} mph, " +
+                   $"Pace: {GetPace():0.0} min per mile";
+        }
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            // Initialize list with activity types
+            List<Activity> activities = new List<Activity>
+            {
+                new Running("03 Nov 2022", 30, 3.0),
+                new Cycling("04 Nov 2022", 45, 12.0),
+                new Swimming("05 Nov 2022", 20, 40)
+            };
+
+            Console.WriteLine("Exercise Tracking Summary:");
+            Console.WriteLine("--------------------------");
+
+            foreach (Activity activity in activities)
+            {
+                // Polymorphism in action: calling the same method name,
+                // but executing different logic based on object type.
+                Console.WriteLine(activity.GetSummary());
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
     }
 }
